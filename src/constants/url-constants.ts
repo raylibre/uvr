04f6e52e6b54c86c017054b1a89451ba0ec92ebc
Mykhailo Usatenko <mykhailou@ext.inditex.com>
2025-06-09 13:57:07 +0300

    [fix] update auth registration endpoint to use correct path

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -1,5 +1,5 @@
 export const V1 = '/v1';
 export const FUNCTIONS = '/functions';
-export const AUTH_REGISTRATION = '/auth-registration';
+export const AUTH_REGISTRATION = '/auth-register';
 
 export const FUNCTIONS_V1_AUTH_REGISTRATION = FUNCTIONS + V1 + AUTH_REGISTRATION;
